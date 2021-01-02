3a870e2bdce8dd5072f19b5423b0781329812afe
BasemElsisy <basemsisy@gmail.com>
2021-01-02 14:51:29 +0200

    feat: create user schema

---
+++ src/user/model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  hashPassword: {
+    type: String,
+    required: true,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+exports.userSchema = userSchema;
